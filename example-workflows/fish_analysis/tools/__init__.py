95bf8063ebdf5b8d52f002ae2871343165419167
Arthur Masson <arthur.sw@gmail.com>
2026-05-06 15:58:28 +0200

    Avoid local tools package re-export side effects

--- example-workflows/fish_analysis/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-"""FISH analysis workflow-specific tools."""
-
-from .download_images import DownloadImages as DownloadImages
-from .average_spots_per_nucleus import AverageSpotsPerNucleus as AverageSpotsPerNucleus
